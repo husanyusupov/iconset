c48694616012eacf901202ece439dea9b3f1d3b9
husanyusupov <husanyusupov@gmail.com>
2021-10-24 20:02:01 +0500

    реализован основной функционал утилиты
    команда iconset принимает два аргумента
    --dir - папка содержащая svg иконки
    --out - папка сгенерированных файлов
    
    Иконки собираются в файл icons.svg. Каждая иконка помещается в тег symbol с уникальным id. id формируется из названия файла иконки. Так как иконки ищутся рекурсивно, то название подпапки добавляется как префикс к id.
    
    А также из идентификаторов иконок создается typescript файл icons.ts который содержит union тип Icons. В значении перечислены все id.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+import chalk from 'chalk';
+import {getArgs} from '../lib/utils.js';
+import run from '../lib/run.js';
+
+const args = getArgs(process.argv.slice(2));
+const calledEmpty = Object.keys(args).length === 0;
+const wrongArguments = !args.dir || args.dir === true || !args.out || args.out === true;
+
+if (calledEmpty || wrongArguments || args.help) {
+  showHelp();
+  process.exit();
+}
+
+const options = {
+  dir: args.dir || './icons',
+  out: args.out || './icons-result',
+};
+
+run(options);
+
+function showHelp() {
+  console.log(`
+    ${chalk.bold("Usage: iconset --dir='svg files directory' --out='destination directory'")}
+    
+    --dir   Svg files directory
+    --out   Destination directory
+    --help  Show help
+  `);
+}
